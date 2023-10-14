3bc3bf38e81db66bbab569aaa321cecd053bc23f
alula-z <azeruesenay14@gmail.com>
2023-10-13 21:46:37 -0400

    Added asset and created some prefabs for player and platforms

---
+++ Ninja Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Left"))
+        {
+            transform.position -= new Vector3(1, 0, 0)* speed * Time.deltaTime;
+        }
+        if (Input.GetButton("Right"))
+        {
+            transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            transform.position += new Vector3(0, 1, 0) * jumpForce * Time.deltaTime;
+        }
+    }
+
+    
+}
