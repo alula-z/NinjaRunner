c7b673545b60707a8d2324a4f213e17903e480ff
alula-z <azeruesenay14@gmail.com>
2023-10-19 18:43:18 -0400

    milestone1

--- Ninja Runner/Assets/Scripts/Player.cs
+++ Ninja Runner/Assets/Scripts/Player.cs
@@ -6,27 +6,50 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         if (Input.GetButton("Left"))
         {
+            anim.SetBool("isMoving", true);
+            anim.SetBool("isJumping", false);
             transform.position -= new Vector3(1, 0, 0)* speed * Time.deltaTime;
+           
         }
         if (Input.GetButton("Right"))
         {
+            anim.SetBool("isMoving", true);
+            anim.SetBool("isJumping", false);
             transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+            
         }
         if (Input.GetButtonDown("Jump"))
         {
-            transform.position += new Vector3(0, 1, 0) * jumpForce * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetBool("isJumping", true);
+            }
+            
+            anim.SetBool("isMoving", false);
+            rb.AddForce(new Vector3(rb.velocity.x, jumpForce));
+
+        }
+
+        if (!Input.anyKey)
+        {
+            anim.SetBool("isMoving", false);
+            anim.SetBool("isJumping", false);
         }
     }
 
